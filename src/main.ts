2e4310456cdbe61eef31ec9d705b65d950568f8c
pegross <me@peter-gross.org>
2020-12-17 01:17:31 +0100

    Test tile rendering and hover effect

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,41 @@
 import * as PIXI from 'pixi.js';
 import Sprite = PIXI.Sprite;
 
+const tileSize = 32;
 
-const app = new PIXI.Application({ width: 256, height: 256 });
+const app = new PIXI.Application({ width: tileSize * 10, height: tileSize * 10 });
 document.body.appendChild(app.view);
 
-app.loader
-    .add('tiles', 'assets/sprites/tiles.json')
-    .load((loader, resources) => {
-        const sprite = new Sprite(resources['grass-bright-1']);
-        app.stage.addChild(sprite);
+const loader = new PIXI.Loader();
+
+loader.add('tiles', 'assets/sprites/tiles.json')
+    .load((l, resources) => {
+        const sheet = resources.tiles;
+
+        if (!sheet || !sheet.textures || !sheet.textures['grass-medium']) {
+            throw new Error('Error loading texture');
+        }
+
+        const texture = sheet.textures['grass-medium'];
+        console.log(texture);
+
+        for (let x = 0; x < 10; x++) {
+            for (let y = 0; y < 10; y++) {
+                const sprite = new Sprite(texture);
+                sprite.x = x * tileSize;
+                sprite.y = y * tileSize;
+                sprite.interactive = true;
+                sprite.on('mouseover', (e: Event) => {
+                    console.log({ 'x': x, 'y': y });
+                    sprite.alpha = 0.9;
+                });
+                sprite.on('mouseout', () => {
+                    sprite.alpha = 1.0
+                });
+
+                app.stage.addChild(sprite);
+            }
+        }
 
     });
-
-
-function setup() {
-    const sheet = app.loader.resources['assets/sprites/tiles.json'];
-
-    console.log(sheet);
-    if (!sheet) {
-        throw new Error(('Sheet undefined'));
-    }
-
-    app.stage.addChild(sprite);
-}
 
